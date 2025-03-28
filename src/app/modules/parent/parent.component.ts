e7ad469a53d307d669c0eddf0af4319a474da4df
Soundharya <Soundharya@cavintek.com>
2025-03-28 17:37:26 +0530

    Signal and defer concept

--- src/app/modules/parent/parent.component.ts
+++ src/app/modules/parent/parent.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'; // Required for standalone components
-import { ContainerResizerComponent } from '../child/child.component';
+
 
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [CommonModule, FormsModule, ContainerResizerComponent], // Import ContainerResizerComponent
+  imports: [CommonModule, FormsModule], // Import ContainerResizerComponent
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {}
+export class ParentComponent {
+  increaseValue() {   
+    this.count.update(value => value + 1);
+  }
+  count = signal(10);
+  count1 = computed(() => this.count() + 1);
+  count2 = computed(() => this.count() + 2);
+}
